137aeea24b3239aedc3651b994252aa634650d9f
Julie Aubry <julie@LAPTOP-6K151MAO.>
2025-04-15 08:18:22 +0200

    feat (bouton input et appel API) ce que j'ai fait aux TP1&2 avant d'avoir fork

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,59 @@
 import logo from './logo.svg';
 import './Home.css';
+import { useState } from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { use } from 'react';
+
+
+function useFetchMovies() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://api.themoviedb.org/3/movie/popular?api_key=57359ff087905e870d40ba4880a1dce0`)
+      .then((response) => {
+        setMovies(response.data.results);
+      })
+      .catch((error) => {
+        // Do something if call failed
+        console.log(error);
+      })
+
+    return;
+  }, []);
+  console.log("useFetchMovies", movies)
+
+  return { movies };
+}
+
+
 
 function Home() {
+  console.log("Début Home")
+  const [movieName, setMovieName] = useState(" ")
+
+  const movies = useFetchMovies();
+
+  console.log("movies", movies)
+  const listMovies = movies.map(movie => <li key={movie.id}>{movie}</li>)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vos films préférés
         </p>
+
+        <label>
+          <input value={movieName} onChange={e => setMovieName(e.target.value)} ></input>
+        </label>
+        <p>
+          {movieName}
+        </p>
+
+        <ul><li key={movies[0].id}>{movies[0]}</li></ul>
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
